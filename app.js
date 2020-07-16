783176d73fba4520c5ee16a358baecce06264986
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-16 15:27:45 -0700

    [callbacks] Add callback functions

--- app.js
+++ app.js
@@ -16,3 +16,50 @@ function anotherGreeting(){
 
 let myName = 'Barent';
 console.log(greeting(myName, anotherGreeting));
+
+
+
+// exercise 1
+function printArray(array, callback){
+    for (let i = 0; i < array.length; i++){
+        let number = array[i]; 
+
+        console.log(number) 
+    }
+
+    callback(array);
+}
+
+function printArrayAgain(array){
+    for (let i = 0; i < array.length; i++){
+        let number = array[i];
+
+        console.log(number);
+
+    }
+}
+
+const numbersArray = [1,2,3,4,5,9,8,7,6];
+
+// printArray(numbersArray, printArrayAgain);
+
+
+// exercise 2
+
+function calculator(num1, num2, callback1, callback2){
+    let result = callback1(num1, num2) + callback2(num1, num2);
+
+    return result
+}
+
+function addNumbers(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+function subtractNumbers(num1, num2){
+    let result = num1 - num2;
+    return result;
+}
+
+console.log(calculator(5, 77, addNumbers, subtractNumbers));
